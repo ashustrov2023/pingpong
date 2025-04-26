5d544ea9c58d9d2a2a9175dda813477b8e4a2e92
ashustrov2023 <129780197+ashustrov2023@users.noreply.github.com>
2025-04-26 13:22:07 +0300

    Add files via upload

---
+++ ping_pong.py
@@ -0,0 +1,41 @@
+from pygame import *
+
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ # конструктор класса
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       # Вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (wight, height))
+       self.speed = player_speed
+
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+
+# класс главного игрока
+class Player(GameSprite):
+   # метод для управления спрайтом стрелками клавиатуры
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    def update_l(self):
+         keys = key.get_pressed()
+         if keys[K_w] and self.rect.y > 5:
+             self.rect.y -= self.speed
+         if keys[K_s] and self.rect.y < win_width - 80:
+             self.rect.y += self.speed
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
