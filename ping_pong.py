1e121067ef649d5ab88e669c81b236de92b3c9a2
ashustrov2023 <129780197+ashustrov2023@users.noreply.github.com>
2025-05-03 12:39:10 +0300

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -39,3 +39,16 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+racket1 = Player('racket1.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket2.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
+
+
